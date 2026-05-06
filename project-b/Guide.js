1aab37380d2eda6df0794ff31ed8fd3ced351478
Moon-Rdy <yd3368@nyu.edu>
2026-05-06 15:44:18 +0800

    update

--- project-b/Guide.js
+++ project-b/Guide.js
@@ -80,7 +80,7 @@ class Guide {
 
     // text
     push();
-    translate(0, height - 50, 0);
+    translate(0, height - 100, 0);
     rotateX(PI/-2.5);
     fill(255, 250, 250, 200);
     noStroke();
