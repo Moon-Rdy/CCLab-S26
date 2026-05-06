37ade2cc6cc34dba48a76ed08863ac46b991c410
Moon-Rdy <yd3368@nyu.edu>
2026-05-06 19:25:05 +0800

    add changes

--- project-b/Guide.js
+++ project-b/Guide.js
@@ -1,6 +1,8 @@
 class Guide {
   constructor() {
     this.guideState = 0;
+    this.gx = 0;
+    this.gy = 0;
   }
 
   update(stage, preStage) {
@@ -20,67 +22,68 @@ class Guide {
   draw() {
     noStroke();
 
-    let a = map(sin(frameCount * 0.07), -1, 1, 130, 180);
+    // let a = map(sin(frameCount * 0.07), -1, 1, 130, 180);
 
-    push();
-    rotateX(PI / -2.5);
+    // push();
+    // rotateX(PI / -2.5);
 
-    if (this.guideState == 0) {
-      fill(255, 255, 0, a);
-    } else {
-      fill(255, 255, 0, 0);
-    }
-    // circle(width / 2 - 30, -height / 2 + 30, 30);
-    triangle(width/2-60, -height/2+50, width/2-50, -height/2+80, width/2-40, -height/2+50);
-    triangle(width/2-50, -height/2+60, width/2-20, -height/2+50, width/2-50, -height/2+40);
-    triangle(width/2-60, -height/2+50, width/2-50, -height/2+20, width/2-40, -height/2+50);
-    triangle(width/2-50, -height/2+60, width/2-80, -height/2+50, width/2-50, -height/2+40);
+    // if (this.guideState == 0) {
+    //   fill(255, 255, 0, a);
+    // } else {
+    //   fill(255, 255, 0, 0);
+    // }
+    
+    // circle(width / 4 , -height / 12, 30);
+    // // triangle(width/4-60, -height/6+50, width/4-50, -height/6+80, width/3-40, -height/6+50);
+    // // triangle(width/4-50, -height/6+60, width/4-20, -height/6+50, width/3-50, -height/6+40);
+    // // triangle(width/4-60, -height/6+50, width/4-50, -height/6+20, width/3-40, -height/6+50);
+    // // triangle(width/4-50, -height/6+60, width/4-80, -height/6+50, width/3-50, -height/6+40);
 
-    if (this.guideState == 1) {
-      fill(255, 255, 0, a);
-    } else {
-      fill(255, 255, 0, 0);
-    }
-    triangle(width/2-60, height/2-50, width/2-50, height/2-80, width/2-40, height/2-50);
-    triangle(width/2-50, height/2-60, width/2-20, height/2-50, width/2-50, height/2-40);
-    triangle(width/2-60, height/2-50, width/2-50, height/2-20, width/2-40, height/2-50);
-    triangle(width/2-50, height/2-60, width/2-80, height/2-50, width/2-50, height/2-40);
+    // if (this.guideState == 1) {
+    //   fill(255, 255, 0, a);
+    // } else {
+    //   fill(255, 255, 0, 0);
+    // }
+    // triangle(width/2-60, height/2-50, width/2-50, height/2-80, width/2-40, height/2-50);
+    // triangle(width/2-50, height/2-60, width/2-20, height/2-50, width/2-50, height/2-40);
+    // triangle(width/2-60, height/2-50, width/2-50, height/2-20, width/2-40, height/2-50);
+    // triangle(width/2-50, height/2-60, width/2-80, height/2-50, width/2-50, height/2-40);
 
-    if (this.guideState == 1.5) {
-      fill(255, 255, 0, a);
-    } else {
-      fill(255, 255, 0, 0);
-    }
-    triangle(-width/2+60, height/2-50, -width/2+50, height/2-80, -width/2+40, height/2-50);
-    triangle(-width/2+50, height/2-60, -width/2+20, height/2-50, -width/2+50, height/2-40);
-    triangle(-width/2+60, height/2-50, -width/2+50, height/2-20, -width/2+40, height/2-50);
-    triangle(-width/2+50, height/2-60, -width/2+80, height/2-50, -width/2+50, height/2-40);
+    // if (this.guideState == 1.5) {
+    //   fill(255, 255, 0, a);
+    // } else {
+    //   fill(255, 255, 0, 0);
+    // }
+    // triangle(-width/2+60, height/2-50, -width/2+50, height/2-80, -width/2+40, height/2-50);
+    // triangle(-width/2+50, height/2-60, -width/2+20, height/2-50, -width/2+50, height/2-40);
+    // triangle(-width/2+60, height/2-50, -width/2+50, height/2-20, -width/2+40, height/2-50);
+    // triangle(-width/2+50, height/2-60, -width/2+80, height/2-50, -width/2+50, height/2-40);
 
-    if (this.guideState == 2) {
-      fill(255, 255, 0, a);
-    } else {
-      fill(255, 255, 0, 0);
-    }
-    triangle(-width/2+60, -height/2+50, -width/2+50, -height/2+80, -width/2+40, -height/2+50);
-    triangle(-width/2+50, -height/2+60, -width/2+20, -height/2+50, -width/2+50, -height/2+40);
-    triangle(-width/2+60, -height/2+50, -width/2+50, -height/2+20, -width/2+40, -height/2+50);
-    triangle(-width/2+50, -height/2+60, -width/2+80, -height/2+50, -width/2+50, -height/2+40);
+    // if (this.guideState == 2) {
+    //   fill(255, 255, 0, a);
+    // } else {
+    //   fill(255, 255, 0, 0);
+    // }
+    // triangle(-width/2+60, -height/2+50, -width/2+50, -height/2+80, -width/2+40, -height/2+50);
+    // triangle(-width/2+50, -height/2+60, -width/2+20, -height/2+50, -width/2+50, -height/2+40);
+    // triangle(-width/2+60, -height/2+50, -width/2+50, -height/2+20, -width/2+40, -height/2+50);
+    // triangle(-width/2+50, -height/2+60, -width/2+80, -height/2+50, -width/2+50, -height/2+40);
 
-    if (this.guideState == 3) {
-      fill(255, 255, 0, a);
-    } else {
-      fill(255, 255, 0, 0);
-    }
-    triangle(width/2-60, -height/2+50, width/2-50, -height/2+80, width/2-40, -height/2+50);
-    triangle(width/2-50, -height/2+60, width/2-20, -height/2+50, width/2-50, -height/2+40);
-    triangle(width/2-60, -height/2+50, width/2-50, -height/2+20, width/2-40, -height/2+50);
-    triangle(width/2-50, -height/2+60, width/2-80, -height/2+50, width/2-50, -height/2+40);
+    // if (this.guideState == 3) {
+    //   fill(255, 255, 0, a);
+    // } else {
+    //   fill(255, 255, 0, 0);
+    // }
+    // triangle(width/2-60, -height/2+50, width/2-50, -height/2+80, width/2-40, -height/2+50);
+    // triangle(width/2-50, -height/2+60, width/2-20, -height/2+50, width/2-50, -height/2+40);
+    // triangle(width/2-60, -height/2+50, width/2-50, -height/2+20, width/2-40, -height/2+50);
+    // triangle(width/2-50, -height/2+60, width/2-80, -height/2+50, width/2-50, -height/2+40);
 
-    pop();
+    // pop();
 
     // text
     push();
-    translate(0, height*0.6, 0);
+    translate(0, height*0.55, 0);
     rotateX(PI/-2.5);
     fill(255, 250, 250, 200);
     noStroke();
--- project-b/sketch.js
+++ project-b/sketch.js
@@ -13,19 +13,24 @@ let video;
 let options = { maxHands: 1, flipped: false };
 let hands = [];
 let p1;
+let sound1;
+let sound2;
+let sound3;
+let sound4;
 
 function preload() {
   handPose = ml5.handPose(options);
   font = loadFont(
     "https://cdnjs.cloudflare.com/ajax/libs/topcoat/0.8.0/font/SourceCodePro-Regular.otf"
   );
+  sound1 = loadSound("audio/1.m4a");
+  sound2 = loadSound("audio/2.m4a");
+  sound3 = loadSound("audio/3.m4a");
+  sound4 = loadSound("audio/4.m4a");
 }
 
 function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight, WEBGL);
-  canvas.id("p5-canvas");
-  canvas.parent("p5-canvas-container");
-
+  createCanvas(windowWidth, windowHeight, WEBGL);
 
   video = createCapture(VIDEO);
   video.size(windowWidth, windowHeight);
@@ -59,93 +64,139 @@ function draw() {
 
   if (W.i == 100) {
     preStage = 1;
+    push();
+      translate(0, height*0.2, 0);
+      rotateX(PI / -2.5);
+      fill(255, 250, 250, 250);
+      noStroke();
+      textSize(11);
+      textAlign(CENTER);
+      textFont(font);
+      text("What do you want for dinner tonight?", 0, 0);
+      text("Whatever you make, I'll eat it — really.", 0, 20);
+      pop();
   }
   if (W.i == 180) {
     preStage = 1.5;
+    push();
+      translate(0, height*0.2, 0);
+      rotateX(PI / -2.5);
+      fill(255, 250, 250, 250);
+      noStroke();
+      textSize(11);
+      textAlign(CENTER);
+      textFont(font);
+      text("Why are you always so careless about these things?", 0, 0);
+      text("Fine fine, just make a whole fish then.", 0, 20);
+      pop();
   }
   if (W.i == 600) {
     preStage = 2;
+      push();
+      translate(0, height*0.2, 0);
+      rotateX(PI / -2.5);
+      fill(255, 250, 250, 250);
+      noStroke();
+      textSize(11);
+      textAlign(CENTER);
+      textFont(font);
+      text("What's for lunch — delivery or go out?", 0, 0);
+      text("Doesn't matter, somewhere near the office is fine.", 0, 20);
+      pop();
   }
   if (W.i == 1000) {
     preStage = 3;
+      push();
+      translate(0, height*0.2, 0);
+      rotateX(PI / -2.5);
+      fill(255, 250, 250, 250);
+      noStroke();
+      textSize(11);
+      textAlign(CENTER);
+      textFont(font);
+      text("Hey, want to grab some snacks after school?", 0, 0);
+      text("Whatever, but I need to be home for dinner.", 0, 20);
+      pop();
   }
 
-  console.log(stage);
+  // console.log(stage);
 
   push();
   rotateX(PI / -2.5);
-  translate(width - width / 2, -height / 2);
+  translate(width / 2, -height / 2, 1);
   scale(-1, 1);
   // image(video,0, 0);
   for (let i = 0; i < hands.length; i++) {
     let hand = hands[i];
     p1 = hand.keypoints[9];
     detectHand();
-    // fill(250, 250, 250);
-    // circle(p1.x, p1.y, 20, 50);
-
-    //blob
-
-    // stroke(255);
     noFill();
     fill(255, 250, 50, 80);
-    // beginShape();
-    // for(let i=0; i< 20; i++){
-    //   let angle = map(i, 0, 20, 0, 2*PI);
-    //   let offset = map(i, 0, 20, 0, 5*PI);
-    //   let r = 10 + 2*sin(frameCount*0.1 + offset);
-    //   let x = p1.x+ r*cos(angle);
-    //   let y = p1.y+ r*sin(angle);
-    //   curveVertex(x, y);
-    // }
-    // endShape(CLOSE);
-    circle(p1.x, p1.y, 50);
+    circle(p1.x, p1.y, 30);
+    // triangle(p1.x-30, p1.y+25, p1.x-25, p1.y+40, p1.x-20, p1.y+25);
+    // triangle(p1.x-25, p1.y+30, p1.x-10, p1.y+25, p1.x-25, p1.y+20);
+    // triangle(p1.x-30, p1.y+25, p1.x-25, p1.y+10, p1.x-20, p1.y+25);
+    // triangle(p1.x-25, p1.y+30, p1.x-40, p1.y+25, p1.x-25, p1.y+20);
 
-    console.log(p1.x);
+    console.log(p1.x, p1.y);
     // console.log(p1.y);
+
+
   }
 
   pop();
+
+  //test
+  // push();
+  // rotateX(PI / -2.5);
+    // fill(255, 0, 0);
+    // circle(250 , 170, 30);
+    // pop();
 }
 
 function detectHand() {
   if (
     stage == 0 &&
     preStage == 0 &&
-    width - p1.x > width - 50 &&
-    width - p1.x < width &&
-    p1.y < 50
+    p1.x > 280 &&
+    p1.x < 340 &&
+    p1.y > 430 &&
+    p1.y < 480
   ) {
     if (stage == 0 && preStage == 0) {
       stage = 1;
       W.stage = 1;
       T.stage = 1;
+      sound1.play();
     }
   } else if (
-    width - p1.x > width - 50 &&
-    width - p1.x < width &&
-    p1.y > height - 50 &&
-    p1.y < height
+   p1.x > 240 &&
+    p1.x < 280 &&
+    p1.y > 450 &&
+    p1.y < 490
   ) {
     if (stage == 1 && preStage == 1 && W.i == 100) {
       W.flip = -1 * W.flip;
       T.stage = 1.5;
       stage = 1.5;
       W.stage = 1.5;
+      sound2.play();
     }
-  } else if (width - p1.x < 50 && p1.y > height - 50 && p1.y < height) {
+  } else if (p1.x > 340 && p1.x < 390 && p1.y > 680 && p1.y < 750) {
     if (stage == 1.5 && preStage == 1.5 && W.i == 180) {
       W.i = 400;
       stage = 2;
       W.stage = 2;
       T.stage = 2;
+      sound3.play();
     }
-  } else if (width - p1.x < 50 && p1.y < 50) {
+  } else if (p1.x > 1150 && p1.x < 1220 && p1.y > 350 && p1.y < 420) {
     if (stage == 2 && preStage == 2 && W.i == 600) {
       W.i = 800;
       stage = 3;
       W.stage = 3;
       T.stage = 3;
+      sound4.play();
     }
   } else if (preStage == 3 && W.i == 1000) {
     W.i = 0;
